48748bc61de83884170b904f16dcc3fecc4263ea
Dana Beschiu <danabeschiu03@gmail.com>
2026-05-14 17:12:01 +0300

    Add e2e test for video screen

---
+++ e2e/tests/test_video_screen.js
@@ -0,0 +1,51 @@
+const rokuLibrary = require("../library/rokuLibrary");
+const expect = require("chai").expect;
+const { spawn } = require('child_process');
+const path = require('path');
+
+const childProcess = spawn('./bin/RokuWebDriver_linux');
+
+let library;
+
+describe('Test video screen', () => {
+    before(async function () {
+        this.timeout(50000);
+        library = new rokuLibrary.Library("192.168.20.108");
+        await library.sideLoad(path.resolve(__dirname, "../../out/hello-world.zip"), "rokudev", "rokudev");
+    });
+
+    it('should launch the channel', async function () {
+        this.timeout(15000);
+        await library.verifyIsChannelLoaded('dev');
+    });
+
+    it('should start playback from the home screen when pressing Play Video button', async function () {
+        this.timeout(30000);
+        await library.sendKey('select');
+        const res = await library.verifyIsPlaybackStarted();
+        expect(res).to.equal(true);
+    });
+
+    it('should return to the home screen when pressing back during playback', async function () {
+        this.timeout(20000);
+        await library.sendKey('back');
+        const res = await library.verifyIsScreenLoaded({
+            'elementData': [{ 'using': 'tag', 'value': 'RowList' }]
+        });
+        expect(res).to.equal(true);
+    });
+
+    it('should start playback from the details screen when pressing Play Video button', async function () {
+        this.timeout(50000);
+        await library.sendKey('down');
+        await library.sendKey('select');
+        await library.sendKey('select', 6);
+        const res = await library.verifyIsPlaybackStarted();
+        expect(res).to.equal(true);
+    });
+
+    after(async () => {
+        await library.close();
+        childProcess.kill();
+    });
+});
